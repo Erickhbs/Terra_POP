f008abf5a2af26bd162c747438e13026171fd94a
Erickhbs <ehbs.13579@gmail.com>
2024-08-14 21:26:38 -0300

    mudanças na interface, style e script

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 async function fetchData() {
     const url = 'https://get-population.p.rapidapi.com/population';
+    
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'a0b3aca417mshc239992fc99a680p11b807jsn62e1a50fd2e1',
+            'X-RapidAPI-Key': 'a0b3aca417mshc239992fc99a680p11b807jsn62e1a50fd2e1',//it's free, you can use it if you want ;)
             'X-RapidAPI-Host': 'get-population.p.rapidapi.com'
         }
-    }
+    };
 
     try {
         const response = await fetch(url, options);
-        const result = await response.text();
 
-        // Seleciona a div com o id "pop"
+        if (!response.ok) {
+            throw new Error(`Erro HTTP: ${response.status}`);
+        }
+
+        const result = await response.json();
+        
         const divPop = document.getElementById('pop');
-
-        // Atualiza o conteúdo da div com os dados recebidos
-        const resultDict = JSON.parse(result)
-        divPop.textContent = resultDict.readable_format;
-    } catch (error) {
-        console.error(error);
+        if (divPop && result && result.readable_format) {
+            divPop.textContent = result.readable_format;
+        } else {
+            console.error('elemento "pop" não encontrado.');
+        }
+    } catch (e) {
+        console.error(e);
     }
 }
 
-setInterval(fetchData, 2000)
+setInterval(fetchData, 2000);
